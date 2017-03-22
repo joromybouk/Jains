7b379a8f05947b851861f0281ab5af29d1ad128e
joromybouk <jb14806@my.bristol.ac.uk>
2017-03-22 22:18:17 +0000

    Setting up complete

---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+import path from 'path'
+import webpack from 'webpack';
+
+export default {
+  entry: [
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+  	filename: 'bundle.js',
+    path: '/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'client'),
+        ],
+        loaders: [ 'babel-loader' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '', '.js' ]
+  }
+}
