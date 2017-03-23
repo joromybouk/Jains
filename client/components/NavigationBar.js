923e7d8be0ee26d09664c8512c1b59c2e0eda28c
joromybouk <jb14806@my.bristol.ac.uk>
2017-03-23 01:42:59 +0000

    Got sign up form template working

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 class NavigationBar extends React.Component {
+
 	render(){
 		return (
 			<nav className="navbar navbar-default">
@@ -15,7 +16,7 @@ class NavigationBar extends React.Component {
           	</ul>
         		</div>
           	</div>
-     	 </nav>
+     	  </nav>
 
 		);
 	}
--- client/components/register/RegisterPage.js
+++ client/components/register/RegisterPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import SignupForm from './SignupForm'
+
 
 class RegisterPage extends React.Component{
 	render(){
 		return(
-			<h1> Register Page </h1>
+			<div className = "row">
+				<div className = "col-md-4 col-md-offset-4">
+					<SignupForm />
+				</div>
+			</div>
 		);
 	}
 }
+export default RegisterPage;
