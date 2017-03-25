866e52cd47a5046a2fb7f94311ceea6cf43be80d
joromybouk <jb14806@my.bristol.ac.uk>
2017-03-25 20:51:59 +0000

    Added error feedback in signup form

--- client/components/register/RegisterPage.js
+++ client/components/register/RegisterPage.js
@@ -6,8 +6,6 @@ import { userRegisterRequest } from '../../actions/registerActions';
 class RegisterPage extends React.Component{
 	render(){
 		const {userRegisterRequest } = this.props;
-
-
 		return(
 			<div className = "row">
 				<div className = "col-md-4 col-md-offset-4">
@@ -17,7 +15,6 @@ class RegisterPage extends React.Component{
 		);
 	}
 }
-
 RegisterPage.propTypes = {
 	userRegisterRequest: React.PropTypes.func.isRequired
 }
--- client/components/register/SignupForm.js
+++ client/components/register/SignupForm.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import timezones from '../../data/timezone';
 import map from 'lodash/map';
+import classnames from 'classnames';
 
 
 class SignupForm extends React.Component{
 	constructor(props){
     	super(props);
     	this.state ={
-      		username : '',
+      		email : '',
       		password : '',
       		passwordConfirmation: '',
-      		timezone: ''
+      		timezone: '',
+      		errors: {},
+      		isLoading : false
 
       	}
       	this.onChange = this.onChange.bind(this);
       	this.onSubmit = this.onSubmit.bind(this);
 	 }
 	 onChange(e){
+	 	//when a text field is modified, update the state
 	 	this.setState({
 	 		[e.target.name] : [e.target.value]
 	 	})
 	 }
 
 	 onSubmit(e){
+	 	//reset error statuses when submission is made again
+	 	this.setState({errors : {}, isLoading: true});
 	 	e.preventDefault();
-	 	this.props.userRegisterRequest(this.state);
+	 	//check for any possible errors with current input
+	 	//if there are errors, they will be displayed on client via render
+	 	this.props.userRegisterRequest(this.state).then(
+	 		() => {},
+	 		({response})=>{this.setState({errors:response.data, isLoading: false})}
+	 	);
 	 }
 
 
 	render(){
+
+		const { errors } = this.state;
+
 		const options = map(timezones, (val,key) =>
 			<option key={val} value={val}>{key}</option>
 		);
@@ -37,18 +51,19 @@ class SignupForm extends React.Component{
 		return(
 			<form onSubmit = {this.onSubmit}>
 				<h1>Join the Jainers!</h1>
-				<div className="form-group">
-					<label className = "control-label">Username</label>
+				<div className={classnames("form-group", { 'has-error': errors.email })}>
+					<label className = "control-label">Email</label>
 					<input
 						value ={this.state.username}
 						onChange= {this.onChange}
 						type= "text"
-						name= "username"
+						name= "email"
 						className = "form-control"
 					/>
+					{errors.email && <span className="help-block">{errors.email}</span>}
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.password })}>
 					<label className = "control-label">Password</label>
 					<input
 						value ={this.state.password}
@@ -57,9 +72,10 @@ class SignupForm extends React.Component{
 						name= "password"
 						className = "form-control"
 					/>
+						{errors.password && <span className="help-block">{errors.password}</span>}
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.passwordConfirmation })}>
 					<label className = "control-label">Confirm Password</label>
 					<input
 						value ={this.state.passwordConfirmation}
@@ -68,9 +84,10 @@ class SignupForm extends React.Component{
 						name= "passwordConfirmation"
 						className = "form-control"
 					/>
+					{errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
 				</div>
 
-				<div className="form-group">
+				<div className={classnames("form-group", { 'has-error': errors.timezone })}>
           			<label className="control-label">Timezone</label>
          				 <select
             				className="form-control"
@@ -81,11 +98,12 @@ class SignupForm extends React.Component{
             	<option value="" disabled>Choose Your Timezone</option>
             	{options}
           		</select>
+          		{errors.timezone && <span className="help-block">{errors.timezone}</span>}
           		</div>
 
 				<div className="form-group">
 				<button
-
+					disabled={this.state.isLoading}
 					className = "btn btn-primary btn-lg">
 					Register
 				</button>
